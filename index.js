0317532e579b2381a439e3975925af3eb6b2945f
rohith <rohithnair2711@gmail.com>
2025-02-21 16:19:26 +0530

    Enhancement/added templates and made each page into a different HTML file

--- index.js
+++ index.js
@@ -1,35 +1,15 @@
 const PORT = location.port;
-const HomePage = `
-<h1>Home Page</h1>
-      <span>
-        While working with <a class="special-keyword" href="https://react.dev/" target="_blank">React</a>, we usually just use the 
-        <a class="special-keyword" href="https://reactrouter.com/en/main/components/browserrouter" target="_blank">Browser Router</a>
-        from React Router without knowing how it works internally.
-      </span>
-      <span>
-        This is just a sample project to understand the internal working of a router in
-        bigger frameworks and understand the history API that each of them uses under the
-        hood. 
-      </span>
-      <span>Go ahead and click the links on the Navbar</span>
-    `;
-const AboutPage = ` <h1>About Page</h1>
-      <span
-        >As you can see the even though we are using anchor tag in the code, the
-        page does not reload and the URL also chages with the help of pushState
-        from history API
-      </span>
-    `;
-const settingPage = `<h1>Setting page</h1>
-      <span>Why do we need a router if we can create SPAs so easily? </span>`;
+const templateLocation = [
+  "./templates/HomePage.html",
+  "./templates/AboutPage.html",
+  "./templates/SettingPage.html",
+];
+
 let root = document.getElementById("root");
-let anchorTags = document.querySelectorAll(".list-item");
-console.log(anchorTags);
-// onClickNavigate();
+
 window.addEventListener("DOMContentLoaded", onClickNavigate);
 
 function onClickNavigate(event) {
-  console.log("called");
   if (event) event.preventDefault();
   // Target will return the whole element and href will only return the url.
   let pathName = "";
@@ -42,16 +22,23 @@ function onClickNavigate(event) {
   window.history.pushState({}, "", event.target.href || location.href);
   pageData(pathName);
 }
-function pageData(pathName) {
+
+// This gets the data from the other html file without reloading
+async function getPageDataFromTemplates(location) {
+  return await fetch(templateLocation[location]).then((response) => {
+    return response.text();
+  });
+}
+async function pageData(pathName) {
   switch (pathName) {
     case "/":
-      root.innerHTML = HomePage;
+      root.innerHTML = await getPageDataFromTemplates(0);
       break;
     case "/about":
-      root.innerHTML = AboutPage;
+      root.innerHTML = await getPageDataFromTemplates(1);
       break;
     case "/setting":
-      root.innerHTML = settingPage;
+      root.innerHTML = await getPageDataFromTemplates(2);
       break;
     default:
       root.innerHTML = "404 not found";
--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.get("*", (req, res) => {
   if (req.url.endsWith(".css")) res.sendFile(path.resolve(cwd, "./index.css"));
   else if (req.url.endsWith(".js"))
     res.sendFile(path.resolve(cwd, "./index.js"));
-  else res.sendFile(path.resolve(cwd, "./index.html"));
+  else if (req.url === "/templates/HomePage.html") {
+    res.sendFile(path.resolve(cwd, "./templates/HomePage.html"));
+  } else if (req.url === "/templates/AboutPage.html") {
+    res.sendFile(path.resolve(cwd, "./templates/AboutPage.html"));
+  } else if (req.url === "/templates/SettingPage.html") {
+    res.sendFile(path.resolve(cwd, "./templates/SettingPage.html"));
+  } else {
+    res.sendFile(path.resolve(cwd, "./index.html"));
+  }
 });
 
 app.listen(5000, () => {
